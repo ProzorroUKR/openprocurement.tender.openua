9e7af56de767d46ebf715de41c900670b7904f38
smithumble <smithumble@gmail.com>
2019-01-21 09:40:08 +0200

    Fix docs tests with scale field

--- docs.py
+++ docs.py
@@ -104,7 +104,8 @@ bid = {
                     "id": u"00137256",
                     "uri": u"http://www.sc.gov.ua/"
                 },
-                "name": "ДКП «Школяр»"
+                "name": "ДКП «Школяр»",
+                "scale": "micro"
             }
         ],
         "subcontractingDetails": "ДКП «книга», Україна, м. Львів, вул. Островського, 33",
@@ -137,7 +138,8 @@ bid2 = {
                     "id": u"00137226",
                     "uri": u"http://www.sc.gov.ua/"
                 },
-                "name": "ДКП «Книга»"
+                "name": "ДКП «Книга»",
+                "scale": "micro"
             }
         ],
         "value": {
@@ -168,7 +170,8 @@ question = {
                 "scheme": "UA-EDR",
                 "uri": "http://sch10.edu.vn.ua/"
             },
-            "name": "ДКП «Школяр»"
+            "name": "ДКП «Школяр»",
+            "scale": "micro"
         },
         "description": "Просимо додати таблицю потрібної калорійності харчування",
         "title": "Калорійність"
@@ -209,7 +212,8 @@ complaint = {
                 "scheme": "UA-EDR",
                 "uri": "http://sch10.edu.vn.ua/"
             },
-            "name": "ДКП «Школяр»"
+            "name": "ДКП «Школяр»",
+            "scale": "micro"
         },
         "description": "Умови виставлені замовником не містять достатньо інформації, щоб заявка мала сенс.",
         "title": "Недостатньо інформації"
